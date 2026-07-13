f5d08ab640062e19d8bd758b6ba72da9319ef977
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-07-13 22:07:33 +0530

    middle-linked-list

---
+++ linkedlist/MiddleLinkedList.py
@@ -0,0 +1,31 @@
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(30)
+head.next.next.next = Node(40)
+head.next.next.next.next = Node(50)
+
+
+def MiddleLinkedList(head):
+    if head == None:
+        return
+
+    count = 0
+    curr = head
+    while curr:
+        curr = curr.next
+        count += 1
+
+    curr = head
+    for _ in range(count//2):
+        curr = curr.next
+    print(curr.key)
+
+
+MiddleLinkedList(head)
