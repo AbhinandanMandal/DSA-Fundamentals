274f49a33b2b5675a00b0f27296547c3ef0b87de
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-07-19 22:04:08 +0530

    circular-linked-list

---
+++ circular linked list/circular_ll.py
@@ -0,0 +1,57 @@
+
+"""
+Circular linked list is of two types,
+1. singly circular linked list
+2. doubly circular linked list
+
+Structure of singly circular linked list is same as normal linked list.
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+
+Example for this can includes, 
+10 -> 20 -> 30 -> 10 (head position itself)
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(30)
+head.next.next.next = head
+
+"""
+A Linked List is a popular data structure which is used to store elements. 
+It is a linear data structure. It contains nodes that have a pointer to store the address 
+of the next node and data which is the value of that node. Circular Linked List is a type of linked list 
+that is circular in nature. In a circular linked list, every node has successor. In this data structure, 
+every node points to the next node and the last node of the linked list points to the first node. 
+This feature makes it circular in nature. It is essential to know that the circular linked lists have no end 
+and we need to be careful while traversing the linked list. 
+
+
+
+Advantages of circular linked list:
+-----------------------------------
+It is possible to traverse from the last node back to the first i.e. the head node.
+The starting node does not matter as we can traverse each and every node despite whatever node we keep as the starting node.
+The previous node can be easily identified.
+There is no need for a NULL function to code. The circular list never identifies a NULL identifier unless it is fully assigned.
+Circular linked lists are beneficial for end operations as start and finish coincide. 
+Algorithms such as Round Robin setup can effectively complete online queues without having to meet NULL suspension or reference checks.
+
+
+Disadvantages of circular linked list:
+---------------------------------------
+If the circular linked list is not handled properly then it can lead to an infinite loop as it is circular in nature.
+In comparison with singly-linked lists, doubly linked lists are more complex in nature
+Direct accessing of elements is not possible.
+It is generally a complex task to reverse a circular linked list.
+
+"""
