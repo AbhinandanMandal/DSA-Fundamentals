1b147fa0decf88e6fadb7e4680a73da3afa4f9ab
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2026-08-06 22:42:53 +0530

    list stack

---
+++ stack/list_stack.py
@@ -0,0 +1,15 @@
+
+# Stack implementation using list
+stack = []
+# append() to push item in stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+print(stack)
+
+# pop() to remove item from stack
+# stack follows LIFO (last in first out manner)
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack)
